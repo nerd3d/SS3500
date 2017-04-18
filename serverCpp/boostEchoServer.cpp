593a8366f95d9384e1ba70c9602e431ba43b4147
aabenson <aabenson@lab1-7.eng.utah.edu>
2017-04-18 09:04:44 -0600

    added a parser to the server

--- serverCpp/boostEchoServer.cpp
+++ serverCpp/boostEchoServer.cpp
@@ -13,8 +13,16 @@
 #include <memory>
 #include <utility>
 #include <boost/asio.hpp>
+#include <vector>
+#include <string.h>
+#include <unordered_map>
 
 using boost::asio::ip::tcp;
+using namespace std;
+
+static unordered_map<int, tcp::socket> mymap;
+static int clientID = 0;
+
 
 class session
   : public std::enable_shared_from_this<session>
@@ -39,7 +47,109 @@ private:
         {
           if (!ec)
           {
-            do_write(length);
+	    cout << "read: " << data_ << endl;
+
+	    
+	    vector<string> words;
+	    string word;
+	    //string checked;
+	    for(int i = 0; i < max_length; i++)
+	      {
+		int j = data_[i];
+
+		//	checked += data_[i];
+		//checked += data_[i+1];
+
+		if(j == 9)//(checked == "  ")
+		  {
+		    words.push_back(word);
+		    word = "";
+		    i++;
+
+		    //  checked = "";
+		  }
+		else if(j == 10)
+		  {
+		    words.push_back(word);
+		    break;
+		  }
+		else
+		  word+=data_[i];
+		
+	      }
+
+	    if(!words[0].compare("Connect"))
+	      {
+		bzero(data_, 301);
+		
+		strcpy(data_, "I want to take you to a gay bar \n");
+		do_write(39);
+	      }
+	    else
+	      do_write(length);
+	    /*
+	    
+	    //	bzero(data_, 301);
+		
+		strcpy(data_, "I want to take you to a gay bar/5/6/70x34A \n");
+		do_write(39);
+	    
+	    */
+	    
+	    /*
+	    int success = 0;
+	    switch(words[0])
+	      {
+	      case"Edit":
+		// Edit\t cellName\t cellContents\t\n
+		// update the spreadsheet
+		// save the updated spreadsheet
+		//	int success;
+		//	success = save(string[1],string[2]);
+
+		//	if(success)
+		  {
+		    // store edit in stack incase someone hits undo(maybe store previous data?)
+		    // update clients
+		  }
+
+		break;
+	      case "Undo":
+		// Undo\t\n
+		// get the top of the stack
+
+		//	success = save(cellname, oldvalue);
+		//	if(success)
+		  {
+		    // pop the stack
+		    // update clients
+		  }
+		break;
+	      case "Connect":
+		// Connect\t spreadsheetName\t\n
+		// send this client the spread sheet
+		// if not  created then create it
+		// add the client to that spreadsheet to that map
+
+		bzero(data_, 301);
+		
+		strcpy(data_, "=> I want to take you to a gay bar\n");
+		do_write(length);
+		break;
+	      case "IsTyping":
+		// IsTyping\t clientID\t cellName\t\n
+		// propogate exact message to all clients 
+		break;
+	      case "DoneTyping":
+		// DoneTyping\t clientID\t cellName\t\n
+		// propogate exact message to all clients
+		break;
+
+
+	      }
+
+	    */
+	    // do_write(length);
           }
         });
   }
@@ -52,12 +162,14 @@ private:
         {
           if (!ec)
           {
+	    cout << "write: " << data_ << endl;
             do_read();
           }
         });
   }
 
   tcp::socket socket_;
+  // enum {Edit, Undo, Connect, IsTyping, DoneTyping}
   enum { max_length = 1024 };
   char data_[max_length];
 };
@@ -80,6 +192,7 @@ private:
         {
           if (!ec)
           {
+	    //  mymap.insert( std::pair<int, tcp::socket>(clientID, socket_));
             std::make_shared<session>(std::move(socket_))->start();
           }
 
@@ -113,4 +226,15 @@ int main(int argc, char* argv[])
   }
 
   return 0;
+}
+
+// saves changes to the spreadsheet returns success or failure.
+int save(string cellName, string cellContents)
+{
+
+}
+
+int updateClients()
+{
+
 }
